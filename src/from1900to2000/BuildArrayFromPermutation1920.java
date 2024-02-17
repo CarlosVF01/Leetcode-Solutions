94a9541feb1fadebaafc05a9bf19ecb0cff31ee4
CarlosVF01 <carlosfornesvela@gmail.com>
2024-02-17 21:08:02 +0100

    Initial commit

---
+++ src/from1900to2000/BuildArrayFromPermutation1920.java
@@ -0,0 +1,14 @@
+package from1900to2000;
+
+public class BuildArrayFromPermutation1920 {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+            int numsLength = nums.length;
+            int[] ans = new int[numsLength];
+            for (int i = 0; i < numsLength; i++) {
+                ans[i] = nums[nums[i]];
+            }
+            return ans;
+        }
+    }
+}
---
+++ src/from1900to2000/ConcatenationOfArray1929.java
@@ -0,0 +1,15 @@
+package from1900to2000;
+
+public class ConcatenationOfArray1929 {
+    class Solution {
+        public int[] getConcatenation(int[] nums) {
+            int numsLength = nums.length;
+            int[] finalArray = new int[numsLength * 2];
+            for (int i = 0; i < numsLength; i++) {
+                finalArray[i] = nums[i];
+                finalArray[i + numsLength] = nums[i];
+            }
+            return finalArray;
+        }
+    }
+}
